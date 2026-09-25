e65ee35e3b09c42e3dd0adc389ac21325be49f4f
che0319-debug <che0319@gmail.com>
2026-09-25 12:15:13 +0800

    Complete pilot prototype src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Home from './pages/Home'
 import Helpers from './pages/Helpers'
 import AIWorkCenter from './pages/AIWorkCenter'
 import AIWorkPilot from './pages/AIWorkPilot'
+import PPTPilotPrototype from './pages/PPTPilotPrototype'
 import LineHY from './pages/LineHY'
 import LineXiaoyin from './pages/LineXiaoyin'
 import Line950157 from './pages/Line950157'
@@ -30,6 +31,7 @@ function DesktopRoutes({ onMobileVersion }) {
         <Route path="memory" element={<Navigate to="/helpers" replace />} />
         <Route path="workspace" element={<AIWorkCenter />} />
         <Route path="ai-work-test" element={<AIWorkPilot />} />
+        <Route path="ai-work-test/ppt-prototype" element={<PPTPilotPrototype />} />
         <Route path="research" element={<Navigate to="/workspace" replace />} />
         <Route path="dispatch" element={<Navigate to="/workspace" replace />} />
         <Route path="line/hy" element={<LineHY />} />
